37644fc057dc38ab1dd5539a1a17d028054cba44
Jyoti Chavan <jyotichavan7447@gmail.com>
2026-09-10 23:47:43 +0530

    Add files via upload

---
+++ StaticKeyword/StaticMethodDemo.java
@@ -0,0 +1,13 @@
+package com.static_keyword_class;
+
+public class StaticMethodDemo {
+
+    static void display() {
+        System.out.println("This is a static method");
+    }
+
+    public static void main(String[] args) {
+
+        display();
+    }
+}
---
+++ StaticKeyword/StaticVariableDemo.java
@@ -0,0 +1,11 @@
+package com.static_keyword_class;
+
+public class StaticVariableDemo {
+	 static int number = 100;
+
+	    public static void main(String[] args) {
+
+	        System.out.println("Number = " + number);
+	    }
+
+}
